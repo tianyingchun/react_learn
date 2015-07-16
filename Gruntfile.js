770172166f3e899dbf707aa17c0f97f66e952167
FuChangjiang <fucj-a@grandsoft.com.cn>
2015-07-16 22:57:48 +0800

    bug fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -94,8 +94,8 @@ module.exports = function(grunt) {
             build: {
                 files: [{
                     expand: true, // Enable dynamic expansion.
-                    cwd: '<%= _modules.cwd %>', // Src matches are relative to this path.
-                    src: ['<%= _modules.bundleDestDir %>/**/*.js'], // Actual pattern(s) to match.
+                    cwd: '<%= _modules.bundleDestDir %>', // Src matches are relative to this path.
+                    src: '**/*.js', // Actual pattern(s) to match.
                     dest: '<%= _modules.bundleDestDir %>', // Destination path prefix.
                     ext: '.min.js', // Dest filepaths will have this extension.
                     extDot: 'first' // Extensions in filenames begin after the first dot
