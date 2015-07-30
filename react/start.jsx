a9550cfc6186b6891083b449e7fb82a893dd0742
tianyingchun <tianyingchun@outlook.com>
2015-07-30 10:48:18 +0800

    updates

---
+++ react/start.jsx
@@ -0,0 +1,28 @@
+/**
+ * @jsx React.DOM
+ */
+
+/**
+ *  For rendering the app on client side.
+ */
+var React = require('react');
+var AppRoutes = require('./AppRoutes.jsx');
+var ReactRouter = require('react-router');
+
+if (typeof window !== 'undefined') {
+    window.onload = function() {
+        ReactRouter
+        // Runs the router, similiar to the Router.run method. You can think of it as an
+        // initializer/constructor method.
+            .create({
+                routes: AppRoutes,
+                location: ReactRouter.HistoryLocation,
+                scrollBehavior: ReactRouter.ScrollToTopBehavior
+            })
+            // This is our callback function, whenever the url changes it will be called again.
+            // Handler: The ReactComponent class that will be rendered
+            .run(function(Handler) {
+                React.render(<Handler />, document);
+            });
+    };
+}
